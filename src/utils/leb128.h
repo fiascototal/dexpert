7b2a60ca1c30ad43d424155f006804b4a4b69a7b
fiascototal <fiascototal@wouaf.org>
2019-05-25 11:15:40 +0200

    add some comment about the LEB 128

--- src/utils/leb128.h
+++ src/utils/leb128.h
@@ -1,6 +1,25 @@
 #ifndef __LEB128_H__
 # define __LEB128_H__
 
+/*
+
+LEB128 ("Little-Endian Base 128") is a variable-length encoding for arbitrary
+signed or unsigned integer quantities. The format was borrowed from the DWARF3
+specification. In a .dex file, LEB128 is only ever used to encode 32-bit
+quantities.
+
+Each LEB128 encoded value consists of one to five bytes, which together
+represent a single 32-bit value. Each byte has its most significant bit set
+except for the final byte in the sequence, which has its most significant bit
+clear. The remaining seven bits of each byte are payload, with the least
+significant seven bits of the quantity in the first byte, the next seven in the
+second byte and so on. In the case of a signed LEB128 (sleb128), the most
+significant payload bit of the final byte in the sequence is sign-extended to
+produce the final value. In the unsigned case (uleb128), any bits not
+explicitly represented are interpreted as 0
+
+*/
+
 # include <stdint.h>
 
 // DECODE the given "buf" into a signed int32_t using the SLEB128 format
