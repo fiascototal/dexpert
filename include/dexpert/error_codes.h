29dbb1e0dfbc22dac4e6c3236ad4f8152a051365
fiascototal <fiascototal@wouaf.org>
2019-05-26 22:27:10 +0200

    add some error codes

---
+++ include/dexpert/error_codes.h
@@ -0,0 +1,9 @@
+#ifndef __ERROR_CODES_H__
+# define __ERROR_CODES_H__
+
+# define ERR_SUCCESS        0
+# define ERR_NO_MEMORY      1
+# define ERR_ITEM_NOT_FOUND 2
+# define ERR_INVALID_ARG    3
+
+#endif/*!__ERROR_CODES_H__*/
