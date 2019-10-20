d696e23954125f136500169637032255700bc1c4
fiascototal <fiascototal@wouaf.org>
2019-10-20 17:32:53 +0200

    update the function to open and read a dex file

--- include/dexpert/dexfile.h
+++ include/dexpert/dexfile.h
@@ -1,6 +1,9 @@
 #ifndef __DEXFILE_H__
 # define __DEXFILE_H__
 
+# include <stdint.h>
+
+
 /*
 ** represent a dexfile object.
 */
@@ -8,9 +11,10 @@ typedef void *dexfile_t;
 
 /*
 ** open the given dex file and build a dexfile_t object (and return it)
-** return NULL on error
+** return 0 on success
 */
-dexfile_t dexfile_open(const char *dex_path);
+int parse_dexfile(const char *dex_path, dexfile_t *new_dex);
+int parse_dexdata(uint8_t *dex_data, uint64_t dex_data_size, dexfile_t *new_dex);
 
 /*
 ** clean/free all structure allocated in the given dexfile_t object
--- include/dexpert/error_codes.h
+++ include/dexpert/error_codes.h
@@ -5,5 +5,6 @@
 # define ERR_NO_MEMORY      1
 # define ERR_ITEM_NOT_FOUND 2
 # define ERR_INVALID_ARG    3
+# define ERR_BUGGY          4
 
 #endif/*!__ERROR_CODES_H__*/
