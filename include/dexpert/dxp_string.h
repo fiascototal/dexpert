c4cbdc302c24970b5e0df7184e4f0e80eb01fce7
fiascototal <fiascototal@wouaf.org>
2019-10-22 23:29:27 +0200

    add some string operators

--- include/dexpert/dxp_string.h
+++ include/dexpert/dxp_string.h
@@ -15,4 +15,18 @@ dxp_string dxp_str_new(uint8_t *data, uint32_t data_size, uint32_t utf16_size);
 // delete a DXP_STRING object
 void dxp_str_del(dxp_string s);
 
+// return the length of the string
+uint32_t dxp_str_len(dxp_string s);
+
+// return the string raw data
+uint8_t dxp_str_data(dxp_string s);
+
+// compare 2 strings (like strcmp)
+# define STR_EQUAL   0
+# define STR_DIFF    1
+# define STR_GREATER 2
+# define STR_LOWER   3
+int dxp_str_eq(dxp_string s1, dxp_string s2);
+int dxp_str_cmp(dxp_string s1, dxp_string s2);
+
 #endif/*!__DXP_STRING_H__*/
--- src/dxp_string.c
+++ src/dxp_string.c
@@ -23,7 +23,8 @@ dxp_string dxp_str_new(uint8_t *data, uint32_t data_size, uint32_t utf16_size)
     result = (struct s_dxp_string *)malloc(sizeof (struct s_dxp_string));
     memset(result, 0, sizeof (struct s_dxp_string));
 
-    result->data = (uint8_t *)malloc(data_size);
+    result->data = (uint8_t *)malloc(data_size + 1);
+    memset(result->data, 0, data_size + 1);
     memcpy(result->data, data, data_size);
     result->utf16_size = utf16_size;
     result->checksum = crc32(data, data_size);
@@ -43,4 +44,45 @@ void dxp_str_del(dxp_string s)
         }
         free(str); str = NULL;
     }
+}
+
+uint32_t dxp_str_len(dxp_string s)
+{
+    struct s_dxp_string *str = (struct s_dxp_string *)s;
+    return (str->utf16_size);
+}
+
+// return the string raw data
+uint8_t dxp_str_data(dxp_string s)
+{
+    struct s_dxp_string *str = (struct s_dxp_string *)s;
+    return (str->data);
+}
+
+// compare 2 strings (like strcmp)
+int dxp_str_eq(dxp_string s1, dxp_string s2)
+{
+    struct s_dxp_string *str1 = (struct s_dxp_string *)s1;
+    struct s_dxp_string *str2 = (struct s_dxp_string *)s2;
+
+    if (str1->checksum == str2->checksum)
+    {
+        if (strcmp((char *)str1->data, (char *)str2->data) == 0)
+            return (STR_EQUAL);
+    }
+    return (STR_DIFF);
+}
+
+// compare 2 strings (like strcmp)
+int dxp_str_cmp(dxp_string s1, dxp_string s2)
+{
+    struct s_dxp_string *str1 = (struct s_dxp_string *)s1;
+    struct s_dxp_string *str2 = (struct s_dxp_string *)s2;
+    int cmp_res = strcmp((char *)str1->data, (char *)str2->data);
+
+    if (cmp_res == 0)
+        return (STR_EQUAL);
+    else if (cmp_res > 0)
+        return (STR_GREATER);
+    return (STR_LOWER);
 }
