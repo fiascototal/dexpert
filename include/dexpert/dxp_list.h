2dda99d7844c620a44b496d981354d5aa9ae8d3f
fiascototal <fiascototal@wouaf.org>
2019-10-26 19:37:28 +0200

    re implement the list structure. It is now an anonymous structure

--- include/dexpert/dxp_list.h
+++ include/dexpert/dxp_list.h
@@ -3,43 +3,34 @@
 
 # include <stdint.h>
 
-struct s_list_item
-{
-    struct s_list_item *next;
-    struct s_list_item *pred;
-    void               *data;
-};
-
-struct s_list
-{
-    struct s_list_item *head;
-    struct s_list_item *tail;
-    uint32_t            size;
-}; 
-
-// the function pointer used to iterate over a given list (see below)
-// if this function return:
-//   0  : continue the iteration
-// > 0  : break the loop 
-typedef int (*f_list_iterator)(
-    struct s_list_item *cur_item,       // the current list item
-    uint32_t            cur_position,   // the current position inside the list
-    void               *priv_data       // some private data, optional
-);
+typedef void *dxp_list;
+typedef void *dxp_list_iterator;
 
 // create a new list object
-struct s_list *list_new();
-
-// add a new item in the list
-int list_insert(struct s_list *list, void *data);
-
-// remove the given item from the list
-int list_delete(struct s_list *list, struct s_list_item *item);
+dxp_list dxp_list_new();
 
 // destroy the whole list
-int list_destroy(struct s_list *list);
+int dxp_list_delete(dxp_list l);
 
-// loop on all items of the givne list. Use the fct pointer to do some stuffs on each items
-int list_iterate(struct s_list *list, f_list_iterator fct, void *priv_data);
+// add a new item in the list (at the end)
+int dxp_list_push(dxp_list l, void *data);
+
+// return the last element and remove it from the list
+void *dxp_list_pop(dxp_list l);
+
+// return the size of the list
+uint32_t dxp_list_length(dxp_list l);
+
+// return a new iterator
+dxp_list_iterator dxp_list_begin(dxp_list l);
+
+// return 1 if the iterator is at the end of the list
+int dxp_list_end(dxp_list_iterator it);
+
+// move the iterator to the next item
+int dxp_list_next(dxp_list_iterator it);
+
+// return the element of the current position of the given iterator
+void *dxp_list_data(dxp_list_iterator it);
 
 #endif/*!__LIST_H__*/
--- src/utils/dxp_list.c
+++ src/utils/dxp_list.c
@@ -1,9 +1,41 @@
-#include "list.h"
-#include "error_codes.h"
 #include <stdlib.h>
+#include <stdint.h>
+
+#include <dexpert/dxp_list.h>
+#include <dexpert/error_codes.h>
 
 
-struct s_list *list_new()
+struct s_list_item
+{
+    struct s_list_item *next;
+    struct s_list_item *pred;
+    void               *data;
+};
+
+struct s_list
+{
+    struct s_list_item *head;
+    struct s_list_item *tail;
+    uint32_t            size;
+}; 
+
+struct s_list_iterator
+{
+    struct s_list      *list;
+    struct s_list_item *current;
+};
+
+
+static void _delete_item(struct s_list_item *item)
+{
+    item->next = NULL;
+    item->pred = NULL;
+    item->data = NULL;
+    free(item);
+}
+
+// create a new list object
+dxp_list dxp_list_new()
 {
     struct s_list *result = NULL;
 
@@ -15,11 +47,49 @@ struct s_list *list_new()
     result->tail = NULL;
     result->size = 0;
 
-    return (result);
+    return ((dxp_list)result);
 }
 
-int list_insert(struct s_list *list, void *data)
+// destroy the whole list
+int dxp_list_delete(dxp_list l)
 {
+    struct s_list      *list    = (struct s_list *)l;
+    struct s_list_item *current = NULL,
+                       *tmp     = NULL;
+
+    if (list == NULL)
+        return (ERR_INVALID_ARG);
+
+    if (list->size > 0)
+    {
+        current = list->head;
+        while (current->next != NULL)
+        {
+            tmp = current;
+            current = current->next;
+
+            if (tmp->data)
+            {
+                free(tmp->data);
+                tmp->data = NULL;
+            }
+
+            _delete_item(tmp);
+        }
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+    free(list); list = NULL;
+
+    return (ERR_SUCCESS);
+}
+
+// add a new item in the list (at the end)
+int dxp_list_push(dxp_list l, void *data)
+{
+    struct s_list      *list = (struct s_list *)l;
     struct s_list_item *item = NULL;
 
     if (list == NULL)
@@ -50,101 +120,100 @@ int list_insert(struct s_list *list, void *data)
     return (ERR_SUCCESS);
 }
 
-int list_delete(struct s_list *list, struct s_list_item *item)
+// return the last element and remove it from the list
+void *dxp_list_pop(dxp_list l)
 {
-    struct s_list_item *current = NULL;
-
-    if (list == NULL || item == NULL)
-        return (ERR_INVALID_ARG);
-
-    if (list->size == 0)
-        return (ERR_ITEM_NOT_FOUND);
-
-    current = list->head;
-    while (current != NULL)
-    {
-        if (current == item)
-        {
-            if (current->pred != NULL)
-                current->pred->next = current->next;
-            if (current->next != NULL)
-                current->next->pred = current->next;
-            if (current == list->head)
-                list->head = current->next;
-            if (current == list->tail)
-                list->tail = current->pred;
-             
-            if (item->data)
-            {
-                free(item->data);
-                item->data = NULL;
-            }
-            item->next = NULL;
-            item->pred = NULL;
-            free(item);
-
-            list->size -= 1;
-            return (ERR_SUCCESS);
-        }
-
-        current = current->next;
-    }
-
-    return (ERR_ITEM_NOT_FOUND);
-}
-
-int list_destroy(struct s_list *list)
-{
-    struct s_list_item *current = NULL,
-                       *tmp = NULL;
+    struct s_list      *list   = (struct s_list *)l;
+    struct s_list_item *item   = NULL;
+    void               *result = NULL;
 
     if (list == NULL)
-        return (ERR_INVALID_ARG);
+        return (NULL);
 
-    if (list->size > 0)
+    if (list->size == 0 || list->tail == NULL)
+        return (NULL);
+    
+    item = list->tail;
+
+    if (list->size == 1)
     {
-        current = list->head;
-        while (current->next != NULL)
-        {
-            tmp = current;
-            current = current->next;
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    else
+    {   
+        list->tail = item->pred;
+        list->tail->next = NULL;
+    }
+    
+    list->size -= 1;
+    result = item->data;
+    _delete_item(item);
 
-            if (tmp->data)
-            {
-                free(tmp->data);
-                tmp->data = NULL;
-            }
-            tmp->next = NULL;
-            tmp->pred = NULL;
-            free(tmp);
-        }
+    return (result);
+}
+
+// return the size of the list
+uint32_t dxp_list_length(dxp_list l)
+{
+    struct s_list *list = (struct s_list *)l;
+    return (list->size);
+}
+
+// return a new iterator
+dxp_list_iterator dxp_list_begin(dxp_list l)
+{
+    struct s_list          *list   = (struct s_list *)l;
+    struct s_list_iterator *result = NULL;
+
+    result = (struct s_list_iterator *)malloc(sizeof (struct s_list_iterator));
+    result->list = list;
+    result->current = list->head;
+
+    return ((dxp_list_iterator)result);
+}
+
+// return 1 if the iterator is at the end of the list
+int dxp_list_end(dxp_list_iterator it)
+{
+    struct s_list_iterator *cur_it = (struct s_list_iterator *)it;
+
+    if (cur_it->current == NULL)
+    {
+        cur_it->current = NULL;
+        cur_it->list = NULL;
+        free(cur_it); cur_it = NULL;
+        return (1);
     }
 
-    list->head = NULL;
-    list->tail = NULL;
-    list->size = 0;
-    free(list);
+    return (0);
+}
+
+// move the iterator to the next item
+int dxp_list_next(dxp_list_iterator it)
+{
+    struct s_list_iterator *cur_it = (struct s_list_iterator *)it;
+    
+    if (cur_it == NULL)
+        return (ERR_INVALID_ARG);
+    if (cur_it->current == NULL)
+        return (ERR_BUGGY);
+    if (cur_it->current->next == NULL)
+        return (ERR_ITEM_NOT_FOUND);
+    cur_it->current = cur_it->current->next;
 
     return (ERR_SUCCESS);
 }
 
-int list_iterate(struct s_list *list, f_list_iterator fct, void *priv_data)
+// return the element of the current position of the given iterator
+void *dxp_list_data(dxp_list_iterator it)
 {
-    struct s_list_item *current = NULL;
-    uint32_t i = 0;
-
-    if (list == NULL || fct == NULL)
+    struct s_list_iterator *cur_it = (struct s_list_iterator *)it;
+    
+    if (cur_it == NULL)
         return (ERR_INVALID_ARG);
+    if (cur_it->current == NULL)
+        return (ERR_BUGGY);
 
-    current = list->head;
-    while (current != NULL)
-    {
-        if (fct(current, i, priv_data) != 0)
-            break;
-
-        i += 1;
-        current = current->next;
-    }
-
-    return (ERR_SUCCESS);
+    return (cur_it->current->data);
 }
