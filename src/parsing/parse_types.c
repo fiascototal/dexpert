6f2c391d06ef8afb0135542153e49960bb1077d6
fiascototal <fiascototal@wouaf.org>
2019-12-21 23:37:10 +0100

    fix a comment

--- src/parsing/parse_types.c
+++ src/parsing/parse_types.c
@@ -20,7 +20,7 @@ int parse_types(struct s_application *app)
         return (1);
     }
 
-    // iterate of the dex to read all strings
+    // iterate of the dex to read all types
     data = app->tmp->data;
     cur_off = app->tmp->hdr->typeIdsOff;
     for (uint32_t i = 0; i < app->tmp->hdr->typeIdsSize; i++)
